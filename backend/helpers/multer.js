888421b8a5d1ce9dd347c623c5d682cf4a3cac4c
Geronimo Nicola <geronicola1696@gmail.com>
2023-04-25 22:32:05 -0300

    quit destination en multer storage

--- backend/helpers/multer.js
+++ backend/helpers/multer.js
@@ -2,7 +2,6 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, "public/uploads"),
     filename: (req, file, cb) => {
         cb(null, new Date().getTime() + path.extname(file.originalname)) // cambiar el nombre de la img
     }
